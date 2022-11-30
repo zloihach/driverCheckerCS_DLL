d45aec9ee57889bef8c671aa25a39f4823c1c21f
zloihach <72695485+zloihach@users.noreply.github.com>
2022-11-30 20:00:12 +0300

    done

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using driveCheckerDLL;
+using static driveCheckerDLL.DriverChecker;
+
+class Test
+{
+    public static void Main()
+    {
+        DriversCounter();
+        DriversInfo();
+        DriversSize();
+        
+    }
+}
