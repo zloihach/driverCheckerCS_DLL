b2c996ecbba1ac5409c2253f6201abe301c2c4b9
zloihach <72695485+zloihach@users.noreply.github.com>
2022-12-01 00:01:33 +0300

    assembly load

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.IO;
-using driveCheckerDLL;
-using static driveCheckerDLL.DriverChecker;
+using System.Reflection;
+using System.Security.AccessControl;
+
+/*using driveCheckerDLL;*/
+/*using static driveCheckerDLL.DriverChecker;*/
 
 class Test
 {
     public static void Main()
     {
-        DriversCounter();
-        DriversInfo();
-        DriversSize();
-        
+        //Указывай полный путь к файлу, хз как по другому работает
+        var dll = Assembly.LoadFile(@"E:\randomwindow_cs\ConsoleApplication2\ConsoleApplication2\driveCheckerDLL.dll");
+        //Достаем из нашей дллки класс
+        var type = dll.GetType("driveCheckerDLL.DriverChecker");
+        //Достаем методы из класса
+        var method = type.GetMethod("DriversInfo");
+        //Просто надо
+        var instance = Activator.CreateInstance(type);
+        //У меня метод без параметров, по этому передаю инстанс и null в качестве параметров
+        var result = method.Invoke(instance, null);
+        //Выводим результат
+        Console.WriteLine(result);
     }
 }
